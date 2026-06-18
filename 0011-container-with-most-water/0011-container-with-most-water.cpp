a6f74b70d6ed4b537c5664d206468b050eed17a9
Mohammed Rehaan <rehaangamer433@gmail.com>
2026-06-18 11:53:46 +0530

    Time: 2172 ms (7.04%), Space: 62.7 MB (99.93%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int max_number = INT_MIN;
+        int max_count = 0;
+        int previous_max = 0;
+        long long max_index = 0;
+        int max_area = INT_MIN;
+        for(int i=0; i<height.size(); i++){
+            max_number = max(height[i], max_number);
+            if(max_number == previous_max){
+
+            }else{
+                max_index = i;
+            }
+            previous_max = max_number;
+        }
+        int distance = 0;
+        int current_area =0;
+        for(int j = 0; j<height.size(); j++){
+            distance = abs((max_index+1)-(j+1));
+            current_area = (height[j])*distance;
+            cout<<current_area<<endl;
+            max_area = max(current_area, max_area);
+        }
+        int k = 0;
+        int l = height.size()-1;
+        while(k!=l){
+            distance = abs((k+1)-(l+1));
+            current_area = ((min(height[k],height[l])*distance));
+            max_area = max(current_area, max_area);
+            cout<<current_area<<endl;
+            if(height[k]<height[l]){
+                k++;
+            }else if(height[k]==height[l]){
+                l--;
+            }else{
+                l--;
+            }
+        }
+       
+        return max_area;
+    }
+};
