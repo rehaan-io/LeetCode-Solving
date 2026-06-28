06dd7e3d771c5b66fe315ad761fa2ad14405c96d
Mohammed Rehaan <rehaangamer433@gmail.com>
2026-06-28 12:39:58 +0530

    Time: 19 ms (0.64%), Space: 11.7 MB (11.35%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i =1; i<nums.size(); i++){
+            int current_num = nums[i];
+            cout<<current_num<<endl;
+            int previous_num = i-1;
+            while(previous_num >= 0 && nums[previous_num]>current_num){
+                nums[previous_num+1] = nums[previous_num];
+                previous_num--;
+            }
+            cout<<current_num<<endl;
+            nums[previous_num+1] = current_num;
+            cout<<nums[previous_num+1]<<endl;
+            cout<<previous_num<<endl;
+        }
+    }
+};
