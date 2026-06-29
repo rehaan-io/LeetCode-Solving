99c609ff3691fa9390f9170419409ed1fe28e4e5
Mohammed Rehaan <rehaangamer433@gmail.com>
2026-06-29 09:09:04 +0530

    Time: 0 ms (100%), Space: 16.1 MB (5.22%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        vector<int> nums2 = nums;
+        int pivot;
+        int swap_val = nums.size()-1;
+        int i = nums.size()-2;
+        while(i>=0){
+            if(nums[i]<nums[i+1]){
+                pivot = i;
+                if(nums[swap_val] == nums[pivot] || nums[swap_val]<nums[pivot]){
+                    swap_val--;
+                    continue;
+                }
+                break;
+            }
+            i--;
+        }
+        swap(nums[pivot], nums[swap_val]);
+        sort(nums.begin()+pivot+1, nums.end());
+    }
+};
