ba0b4a45086ed504c301d86e5879aa4720e9bf20
Mohammed Rehaan <rehaangamer433@gmail.com>
2026-07-13 18:44:19 +0530

    Time: 59 ms (20.89%), Space: 29.1 MB (47.61%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,40 +3,39 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
 
-        vector<vector<int>> ans;
         int n = nums.size();
+        vector<vector<int>> ans;
 
-        for (int i = 0; i < n - 2; i++) {
-
+        for (int i = 0; i < n; i++) {
             // Skip duplicate first elements
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            int left = i + 1;
-            int right = n - 1;
+            int j = i + 1;
+            int k = n - 1;
 
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
 
-                if (sum == 0) {
-                    ans.push_back({nums[i], nums[left], nums[right]});
-
-                    left++;
-                    right--;
-
-                    // Skip duplicate left values
-                    while (left < right && nums[left] == nums[left - 1])
-                        left++;
-
-                    // Skip duplicate right values
-                    while (left < right && nums[right] == nums[right + 1])
-                        right--;
+                if (sum < 0) {
+                    j++;
                 }
-                else if (sum < 0) {
-                    left++;
+                else if (sum > 0) {
+                    k--;
                 }
                 else {
-                    right--;
+                    ans.push_back({nums[i], nums[j], nums[k]});
+
+                    j++;
+                    k--;
+
+                    // Skip duplicate second elements
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+
+                    // Skip duplicate third elements
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
                 }
             }
         }
